4a4bdbb70041fb2be8b95ef2394d0768eb326b47
Mustafa Khoraidah <mhk-1993@hotmail.com>
2021-12-27 03:30:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-
 import plotly.express as px
 import plotly.graph_objects as go
 from jupyter_dash import JupyterDash
-
-#import dash_core_components as dcc
 from dash import dcc
-
-#import dash_html_components as html
 from dash import html
 from dash.dependencies import Input, Output
-
 import dash
 import dash_bootstrap_components as dbc
-#from dash.dependencies import Input, Output
 from dash import Input, Output, dcc, html
 
 
